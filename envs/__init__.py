b480c215b1e5a257784dc11248c347ef5d0c3cd4
mauricio <mauriciogtec@gmail.com>
2025-01-02 22:50:44 -0500

    gym en vregistration

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,18 +1,16 @@
 from envs.finance import BuySellHoldLang
 from envs.heat_alerts import HeatAlertsLang
 from envs.vital_signs import VitalSignsLang
+from gymnasium.envs.registration import register
 
+uganda = lambda: VitalSignsLang("models/uganda.npz")
+mimic_iii = lambda: VitalSignsLang("models/mimic-iii.npz")
+mimic_iv = lambda: VitalSignsLang("models/mimic-iv.npz")
+heat_alerts = lambda: HeatAlertsLang()
+buy_sell_hold = lambda: BuySellHoldLang()
 
-if __name__ == "__main__":
-    import sys
-
-    env1 = BuySellHoldLang()
-    print(env1.task_text)
-
-    env2 = HeatAlertsLang()
-    print(env2.task_text)
-
-    env3 = VitalSignsLang("models/uganda.npz")
-    print(env3.task_text)
-
-    sys.exit(0)
+register(id="uganda", entry_point="envs:uganda")
+register(id="mimic-iii", entry_point="envs:mimic_iii")
+register(id="mimic-iv", entry_point="envs:mimi_iv")
+register(id="buy_sell_hold", entry_point="envs:BuySellHoldLang")
+register(id="heat_alerts", entry_point="envs:HeatAlertsLang")
