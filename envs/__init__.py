fcef96209919fd80900be659eb3099ef96c2aea5
mauricio <mauriciogtec@gmail.com>
2025-01-05 20:29:24 -0500

    add simplified buy sell env to gym make

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,16 +1,21 @@
-from envs.finance import BuySellHoldLang
+from envs.buy_sell import BuySellTextLang, BuySellLang
 from envs.heat_alerts import HeatAlertsLang
 from envs.vital_signs import VitalSignsLang
 from gymnasium.envs.registration import register
+from functools import partial
 
-uganda = lambda: VitalSignsLang("models/uganda.npz")
-mimic_iii = lambda: VitalSignsLang("models/mimic-iii.npz")
-mimic_iv = lambda: VitalSignsLang("models/mimic-iv.npz")
-heat_alerts = lambda: HeatAlertsLang()
-buy_sell_hold = lambda: BuySellHoldLang()
+Uganda = partial(VitalSignsLang, "models/uganda.npz")
+MimicIII = partial(VitalSignsLang, "models/mimic-iii.npz")
+MimicIV = partial(VitalSignsLang, "models/mimic-iv.npz")
+HeatAlerts = partial(HeatAlertsLang)
+BuySellText = partial(BuySellTextLang)
+BuySell2 = partial(BuySellLang)
 
-register(id="uganda", entry_point="envs:uganda")
-register(id="mimic-iii", entry_point="envs:mimic_iii")
-register(id="mimic-iv", entry_point="envs:mimi_iv")
-register(id="buy_sell_hold", entry_point="envs:BuySellHoldLang")
-register(id="heat_alerts", entry_point="envs:HeatAlertsLang")
+
+kwargs = {"disable_env_checker": True, "order_enforce": False}
+register(id="Uganda", entry_point="envs:Uganda", **kwargs)
+register(id="MimicIII", entry_point="envs:MimicIII", **kwargs)
+register(id="MimicIV", entry_point="envs:MimicIV", **kwargs)
+register(id="HeatAlerts", entry_point="envs:HeatAlerts", **kwargs)
+register(id="BuySellText", entry_point="envs:BuySellText", max_episode_steps=20, **kwargs)
+register(id="BuySell", entry_point="envs:BuySell2", max_episode_steps=20, **kwargs)
