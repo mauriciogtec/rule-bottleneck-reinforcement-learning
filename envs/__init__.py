f958e7c7a267e92a39fd27dd0e8c0d5487746a4e
mauricio <mauriciogtec@gmail.com>
2025-01-07 15:29:10 -0500

    Adds simplified vital signs env.

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,15 +1,14 @@
-from envs.buy_sell import BuySellTextLang, BuySellLang
+from envs.buy_sell import BuySellSimpleLang
 from envs.heat_alerts import HeatAlertsLang
-from envs.vital_signs import VitalSignsLang
+from envs.vital_signs import VitalSignsSimpleLang
 from gymnasium.envs.registration import register
 from functools import partial
 
-Uganda = partial(VitalSignsLang, "models/uganda.npz")
-MimicIII = partial(VitalSignsLang, "models/mimic-iii.npz")
-MimicIV = partial(VitalSignsLang, "models/mimic-iv.npz")
+Uganda = partial(VitalSignsSimpleLang, "models/uganda.npz")
+MimicIII = partial(VitalSignsSimpleLang, "models/mimic-iii.npz")
+MimicIV = partial(VitalSignsSimpleLang, "models/mimic-iv.npz")
 HeatAlerts = partial(HeatAlertsLang)
-BuySellText = partial(BuySellTextLang)
-BuySell2 = partial(BuySellLang)
+BuySellSimple = partial(BuySellSimpleLang)
 
 
 # kwargs = {"disable_env_checker": True, "order_enforce": False}
@@ -18,5 +17,4 @@ register(id="Uganda", entry_point="envs:Uganda", **kwargs)
 register(id="MimicIII", entry_point="envs:MimicIII", **kwargs)
 register(id="MimicIV", entry_point="envs:MimicIV", **kwargs)
 register(id="HeatAlerts", entry_point="envs:HeatAlerts", **kwargs)
-register(id="BuySellText", entry_point="envs:BuySellText", max_episode_steps=16, **kwargs)
-register(id="BuySell", entry_point="envs:BuySell2", max_episode_steps=16, **kwargs)
+register(id="BuySellSimple", entry_point="envs:BuySellSimple", max_episode_steps=16, **kwargs)
