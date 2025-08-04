7cf139db28d061bb28b83ba18fff2b369e513e7a
mauriciogtec <mauriciogtec@gmail.com>
2025-08-04 02:36:59 +0000

    high temperature for rules

--- agents.py
+++ agents.py
@@ -524,7 +524,7 @@ def _gen_rules(
             )
 
         tmp_messages = [{"role": "user", "content": rules_prompt}]
-        response = invoke_with_retries(llm, tmp_messages, max_tokens=512, temperature=0.9, n=num_rules).content
+        response = invoke_with_retries(llm, tmp_messages, max_tokens=512, temperature=2.0, n=num_rules).content
         # rules = parse_rules(response)
         # for each rlue eliminate all thoughts in form <think></think>
         rules = [re.sub(r"<think>.*?</think>", "", l) for l in response]
